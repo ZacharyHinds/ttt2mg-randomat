d0279ca8e6b111e6e5c75ea0fcf7ce2cb9f9a6a0
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-07-22 18:39:21 -0400

    Create sh_fov_minigame.lua

---
+++ lua/minigames/minigames/sh_fov_minigame.lua
@@ -0,0 +1,60 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {
+  ttt2_minigames_fov_scale = {
+    slider = true,
+    min = 0,
+    max = 3,
+    decimal = 1,
+    desc = "(Def. 1.5)"
+  }
+}
+
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "Quake Pro"
+    },
+    desc = {
+      English = ""
+    }
+  }
+else
+  ttt2_minigames_fov_scale = CreateConVar("ttt2_minigames_fov_scale", "1.5", {FCVAR_ARCHIVE}, "Scale of fov increase")
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    starting_plys = {}
+    local changed_fov = {}
+
+    for k, ply in ipairs(player.GetAll()) do
+      starting_plys[k] = ply
+      changed_fov[ply] = ply:GetFOV() * ttt2_minigames_fov_scale:GetFloat()
+    end
+
+    timer.Create("FOVMinigame", 0.1, 0, function()
+      for _, ply in pairs(starting_plys) do
+        if ply:Alive() and not ply:IsSpec() then
+          ply:SetFOV(changed_fov[ply], 0)
+        else
+          ply:SetFOV(0, 0)
+        end
+      end
+    end)
+  end
+
+  function MINIGAME:OnDeactivation()
+    timer.Remove("FOVMinigame")
+
+    for _, ply in pairs(starting_plys) do
+      ply:SetFOV(0, 0)
+    end
+  end
+end
