1e114e6f59c4745f5afb8c0ec5cc00137c047bee
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-07-22 18:02:27 -0400

    Create sh_nofall_minigames.lua

---
+++ lua/minigames/minigames/sh_nofall_minigames.lua
@@ -0,0 +1,30 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "No Fall Damage!"
+    },
+    desc = {
+      English = ""
+    }
+  }
+else
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    hook.Add("EntityTakeDamage", "NoFallMinigame", function(ply, dmginfo)
+      if IsValid(ply) and ply:IsPlayer() and dmginfo:IsFallDamage() then return true end
+    end)
+  end
+
+  function MINIGAME:OnDeactivation()
+    hook.Remove("EntityTakeDamage", "NoFallMinigame")
+  end
+end
