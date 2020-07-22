1863a30c05d1096487110b7721411f513c7b1c59
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-07-22 16:16:22 -0400

    Create sh_blind_minigame.lua

---
+++ lua/minigames/minigames/sh_blind_minigame.lua
@@ -0,0 +1,84 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {
+  ttt2_minigames_blind_duration = {
+    slider = true,
+    min = 1,
+    max = 60,
+    desc = "(Def. 30)"
+  }
+}
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "Blinded!"
+    },
+    desc = {
+      English = "The Traitors have been temporarily blinded!"
+    }
+  }
+else
+  ttt2_minigames_blind_duration = CreateConVar("ttt2_minigames_blind_duration", "30", {FCVAR_ARCHIVE}, "Duration of the traitor's blindness")
+  util.AddNetworkString("blind_minigame")
+end
+
+
+if SERVER then
+  function TriggerBlind()
+    net.Start("blind_minigame")
+    net.WriteBool(true)
+    net.Broadcast()
+
+    local duration = ttt2_minigames_blind_duration:GetInt()
+
+    timer.Create("MinigameBlind", duration, 1, function()
+      net.Start("blind_minigame")
+      net.WriteBool(false)
+      net.Broadcast()
+    end)
+  end
+
+  function RemoveBlind()
+    net.Start("blind_minigame")
+    net.WriteBool(false)
+    net.Broadcast()
+  end
+
+  function MINIGAME:OnActivation()
+    TriggerBlind()
+  end
+
+  function MINIGAME:OnDeactivation()
+    RemoveBlind()
+  end
+end
+
+if CLIENT then
+  blind_active = false
+
+  net.Receive("blind_minigame", function()
+    if net.ReadBool() then
+      blind_active = true
+    else
+      blind_active = false
+    end
+  end)
+
+  function BlindPlayer()
+    if not blind_active then return end
+    local client = LocalPlayer()
+
+    if client:HasTeam(TEAM_TRAITOR) then
+      surface.SetDrawColor(0, 0, 0, 255)
+      surface.DrawRect(0, 0, surface.ScreenWidth(), surface.ScreenHeight())
+    end
+  end
+
+  hook.Add("HUDPaint", "BlindMinigame", BlindPlayer)
+end
