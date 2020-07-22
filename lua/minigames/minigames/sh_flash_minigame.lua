4644023ec9f54cd85a152c1143fe67f1aa301ded
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-07-22 18:16:57 -0400

    Update sh_flash_minigame.lua

---
+++ lua/minigames/minigames/sh_flash_minigame.lua
@@ -0,0 +1,40 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+MINIGAME.conVarData = {
+  ttt2_minigames_flash_scale = {
+    slider = true,
+    min = 1,
+    max = 100,
+    desc = "(Def. 50)"
+  }
+}
+
+ttt2_minigames_flash_scale = CreateConVar("ttt2_minigames_flash_scale", "50", {FCVAR_ARCHIVE}, "Multiplier for time scale")
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "Speeeeeed!"
+    },
+    desc = {
+      English = "Everthing is fast as Flash now! (" .. ttt2_minigames_flash_scale:GetInt() .. "% faster)"
+    }
+  }
+else
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    ts = game.GetTimeScale()
+    game.SetTimeScale(ts + (ttt2_minigames_flash_scale:GetInt() / 100))
+  end
+
+  function MINIGAME:OnDeactivation()
+    game.SetTimeScale(1)
+  end
+end
