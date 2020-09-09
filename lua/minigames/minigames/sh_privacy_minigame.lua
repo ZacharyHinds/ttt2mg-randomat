82055710092a4ba8f0737bbfef264fd0e0258228
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-09 19:48:53 -0400

    Switched to use EPOP

--- lua/minigames/minigames/sh_privacy_minigame.lua
+++ lua/minigames/minigames/sh_privacy_minigame.lua
@@ -2,8 +2,6 @@ if SERVER then
   AddCSLuaFile()
 end
 
--- include("enhancednotificationscore/shared.lua")
-
 MINIGAME.author = "Wasted"
 MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
 
@@ -23,8 +21,7 @@ end
 if SERVER then
   function MINIGAME:OnActivation()
     hook.Add("TTTOrderedEquipment", "PrivacyMinigameNotification", function(ply, equipment, is_item)
-        rolename = ply:GetRoleString()
-        rolename = string.gsub("^%a", string.upper)
+        local rolename = ply:GetRoleString()
 
         net.Start("privacy_mg_notif")
         net.WriteString(rolename)
@@ -40,61 +37,16 @@ if SERVER then
 
   end
 elseif CLIENT then
-  local tbl = {}
-  local role = {}
-  local function GetEquipmentTranslation(name, printName)
-    local SafeTranslation = LANG.TryTranslation
-    local translatedText = SafeTranslation(printName)
-    if translatedText == printName and name then
-      translatedText = SafeTranslate(name)
-    end
-
-    if translatedText == name and printName then
-      translatedText = printName
-    end
-
-    return translatedText
-  end
-
   net.Receive("privacy_mg_notif", function()
     local rolename = net.ReadString()
-    local ply = net.ReadEntity()
     local rolecolor = net.ReadTable()
     local equipment = net.ReadString()
-    local is_item = net.ReadBool()
 
-    local curRole = ply:GetSubRole()
-
-    if tbl == nil or role ~= curRole then
-      tbl = GetEquipmentForRole(ply, curRole, true)
-      role = curRole
-    end
-
-    local itemObject = nil
-
-    if is_item then
-      for _, item in pairs(tbl) do
-        itemObject = item
-        break
-      end
-    else
-      itemObject = weapons.GetStored(equipment)
-    end
-
-    local itemName = itemObject and GetEquipmentTranslation(itemObject.name, itemObject.PrintName) or "Undefined"
-    local itemMaterial = itemObject and (itemObject.material or itemObject.Icon) or "vgui/ttt/tbn_ic_default"
-
-    local bgColor = Color(255, 0, 0)
-
-    bgColor = rolecolor
-
-    bgColor.a = 240
-
-    local message = 'bought "' .. itemName .. '"'
-    local mat = Material(itemMaterial)
-    MSTACK:AddImagedMessage(message, mat, rolename)
-
-    chat.AddText(Color(255, 255, 255), rolename, Color(200, 200, 200), " bought ", Color(255, 255, 255), itemName)
+    EPOP:AddMessage({
+      text = rolename .. " bought " .. equipment,
+      color = rolecolor},
+      "",
+      2
+    )
   end)
-
 end
