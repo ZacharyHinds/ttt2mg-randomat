124eacd916a83d8fdba0c85afab30846ffa19003
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-10 15:44:14 -0400

    Improved player for-loops

--- lua/minigames/minigames/sh_cantstop_minigame.lua
+++ lua/minigames/minigames/sh_cantstop_minigame.lua
@@ -26,14 +26,11 @@ else
 end
 
 if SERVER then
+  local plys = player.GetAll()
   function MINIGAME:OnActivation()
-    local plys = {}
-    for k, ply in ipairs(player.GetAll()) do
-      plys[k] = ply
-    end
-
     hook.Add("Think", "CantStopMinigame", function()
-      for _, ply in ipairs(player.GetAll()) do
+      for i = 1, #plys do
+        local ply = plys[i]
         if ply:Alive() and not ply:IsSpec() then
           ply:ConCommand("+forward")
           if ttt2_minigames_cantstop_disable_back:GetBool() then
@@ -41,7 +38,6 @@ if SERVER then
           end
         else
           ply:ConCommand("-forward")
-          plys[_] = nil
         end
       end
     end)
@@ -49,8 +45,8 @@ if SERVER then
 
   function MINIGAME:OnDeactivation()
     hook.Remove("Think", "CantStopMinigame")
-    for _, ply in ipairs(player.GetAll()) do
-      ply:ConCommand("-forward")
+    for i = 1, #plys do
+      plys[i]:ConCommand("-forward")
     end
   end
 end
