1f4949346f2f08d535290d0944bc3a7833cda3a9
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-12 20:27:36 -0400

    Butter Improvements

--- lua/minigames/minigames/sh_butter_minigame.lua
+++ lua/minigames/minigames/sh_butter_minigame.lua
@@ -33,26 +33,31 @@ if SERVER then
   local ttt2_minigames_butter_timer = CreateConVar("ttt2_minigames_butter_timer", "10", {FCVAR_ARCHIVE}, "Time between each weapon drop")
   local ttt2_minigames_butter_affectall = CreateConVar("ttt2_minigames_butter_affectall", "0", {FCVAR_ARCHIVE}, "Should butterfingers affect everyone at once")
   function MINIGAME:OnActivation()
-    local plys = util.GetAlivePlayers()
     timer.Create("ButterMinigame", ttt2_minigames_butter_timer:GetInt(), 0, function()
+      local plys = util.GetAlivePlayers()
       if ttt2_minigames_butter_affectall:GetBool() then
         for i = 1, #plys do
           local ply = plys[i]
-          if not ply:Alive() or ply:IsSpec() then continue end
+          local wep = ply:GetActiveWeapon()
+          if not IsValid(wep) then continue end
 
-          if ply:GetActiveWeapon().AllowDrop then
-            ply:DropWeapon(ply:GetActiveWeapon())
+          if wep.AllowDrop then
+            ply:DropWeapon(wep)
             ply:SetFOV(0, 0.2)
             ply:EmitSound("vo/npc/Barney/ba_pain01.wav")
           end
         end
       else
-        local ply = plys[math.random(#plys)]
-        while not ply:Alive() or ply:IsSpec() do
-          ply = plys[math.random(#plys)]
-        end
-        if ply:GetActiveWeapon().AllowDrop then
-          ply:DropWeapon(ply:GetActiveWeapon())
+        repeat
+          if #plys <= 0 then return end
+          local rnd = math.random(#plys)
+          local ply = plys[rnd]
+          table.remove(plys, rnd)
+        until IsValid(ply)
+        local wep = ply:GetActiveWeapon()
+        if not IsValid(wep) then return end
+        if wep.AllowDrop then
+          ply:DropWeapon(wep)
           ply:SetFOV(0, 0.2)
           ply:EmitSound("vo/npc/Barney/ba_pain01.wav")
         end
