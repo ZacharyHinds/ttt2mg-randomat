91777945bcd47f8cb094936c0636710a64beb622
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-14 14:13:51 -0400

    Better player selection, Angel removal on deactivation

--- lua/minigames/minigames/sh_blink_minigame.lua
+++ lua/minigames/minigames/sh_blink_minigame.lua
@@ -35,17 +35,21 @@ end
 if SERVER then
   local ttt2_minigames_blink_cap = CreateConVar("ttt2_minigames_blink_cap", "12", {FCVAR_ARCHIVE}, "Max angels to spawn")
   local ttt2_minigames_blink_delay = CreateConVar("ttt2_minigames_blink_delay", "0.5", {FCVAR_ARCHIVE}, "Delay between angel spawns")
+  local angels = {}
   function MINIGAME:OnActivation()
     if ttt2_minigames_blink_cap:GetInt() <= 0 then return end
+    local plys = util.GetAlivePlayers()
     timer.Create("AngelMinigame", ttt2_minigames_blink_delay:GetFloat(), ttt2_minigames_blink_cap:GetInt(), function()
-      local plys = util.GetAlivePlayers()
       local ply
       repeat
-        if #plys <= 0 then return end
+        if #plys <= 0 then
+          plys = util.GetAlivePlayers()
+          if #plys <= 0 then return end
+        end
         local rnd = math.random(#plys)
         ply = plys[rnd]
         table.remove(plys, rnd)
-      until IsValid(ply) and ply:Alive() and not ply:IsSpec()
+      until IsValid(ply)
 
       local ent = ents.Create("weepingangel")
       if not IsValid(ent) then return end
@@ -54,6 +58,7 @@ if SERVER then
       ent:Activate()
       ent:SetVictim(ply)
       ent:DropToFloor()
+      angels[#angels + 1] = ent
     end)
   end
 
@@ -63,5 +68,8 @@ if SERVER then
 
   function MINIGAME:OnDeactivation()
     timer.Remove("AngelMinigame")
+    for i = 1, #angels do
+      angels[i]:Remove()
+    end
   end
 end
