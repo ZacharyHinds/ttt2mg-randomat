cc1421a52b5385b0b6075b5994cc2529a4e6d093
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-17 17:34:45 -0400

    Use item_ttt_infinishoot if installed

--- lua/minigames/minigames/sh_ammo_minigame.lua
+++ lua/minigames/minigames/sh_ammo_minigame.lua
@@ -19,15 +19,22 @@ end
 
 if SERVER then
   function MINIGAME:OnActivation()
-    hook.Add("EntityFireBullets", "MinigameAmmo", function(ply, data)
-      if not IsValid(ply) or not ply:IsPlayer() then return end
-      local wep = ply:GetActiveWeapon()
+    if items.GetStored("item_ttt_infinishoot") then
+      local plys = util.GetAlivePlayers()
+      for i = 1, #plys do
+        plys[i]:GiveEquipmentItem("item_ttt_infinishoot")
+      end
+    else
+      hook.Add("EntityFireBullets", "MinigameAmmo", function(ply, data)
+        if not IsValid(ply) or not ply:IsPlayer() then return end
+        local wep = ply:GetActiveWeapon()
 
-      if not wep.HasAmmo or not wep:HasAmmo() or not wep.AutoSpawnable or (wep.Kind == WEAPON_EQUIP1 or wep.Kind == WEAPON_EQUIP2) then return end
+        if not wep.HasAmmo or not wep:HasAmmo() or not wep.AutoSpawnable or (wep.Kind == WEAPON_EQUIP1 or wep.Kind == WEAPON_EQUIP2) then return end
 
-      wep.inf_clip_old = wep:Clip1()
-      wep:SetClip1(wep:GetMaxClip1() + 1)
-    end)
+        wep.inf_clip_old = wep:Clip1()
+        wep:SetClip1(wep:GetMaxClip1() + 1)
+      end)
+    end
   end
 
   function MINIGAME:OnDeactivation()
