65eb1a12a9575192e7cc042f2fb407c22044c094
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-21 16:54:50 -0400

    Update to use minigames.Select()

--- lua/minigames/minigames/sh_intensifies_minigame.lua
+++ lua/minigames/minigames/sh_intensifies_minigame.lua
@@ -28,47 +28,10 @@ end
 
 if SERVER then
   local ttt2_minigames_intensifies_timer = CreateConVar("ttt2_minigames_intensifies_timer", "20", {FCVAR_ARCHIVE}, "How often should a new game be activated")
-  function MINIGAME:SelectGame()
-    local mgs = minigames.GetList()
-
-    if #mgs == 0 then return end
-
-    local availableMinigames = {}
-    local forcedNextMinigame = minigames.GetForcedNextMinigame()
-
-    for i = 1, #mgs do
-      local minigame = mgs[i]
-
-      if not GetConVar("ttt2_minigames_" .. minigame.name .. "_enabled"):GetBool() or not minigame:IsSelectable() then continue end
-
-      if forcedNextMinigame and forcedNextMinigame.name == minigame.name then
-        forcedNextMinigame = nil
-        return minigame
-      end
-
-      local b = true
-
-      local r = GetConVar("ttt2_minigames_" .. minigame.name .. "_random"):GetInt()
-
-      if r > 0 and r < 100 then
-        b = math.random(100) <= r
-      elseif r <= 0 then
-        b = false
-      end
-
-      if b then
-        availableMinigames[#availableMinigames + 1] = minigame
-      end
-    end
-
-    if #availableMinigames == 0 then return end
-
-    return availableMinigames[math.random(#availableMinigames)]
-  end
 
   function MINIGAME:OnActivation()
     timer.Create("IntensifiesMinigame", ttt2_minigames_intensifies_timer:GetInt(), 0, function()
-      local minigame = self:SelectGame()
+      local minigame = minigames.Select()
       if not minigame then return end
 
       ActivateMinigame(minigame)
