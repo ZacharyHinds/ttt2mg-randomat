4e47f99e4a88b7d78a16b55cd8026aa897df2c06
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 22:19:53 +0500

    Create randomat_mg.lua

---
+++ lua/terrortown/lang/russian/randomat_mg.lua
@@ -0,0 +1,52 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+--Communism
+L["ttt2mg_communism_epop"] = "{role} купил всем {item}"
+
+--Explode
+L["ttt2mg_explode_epop"] = "{nick} взорвался!"
+
+--Explosive Traitor
+L["ttt2mg_texplode_countdown_30"] = "30 секунд до взрыва предателя"
+L["ttt2mg_texplode_countdown_10"] = "Осталось 10 секунд!"
+L["ttt2mg_texplode_explode"] = "Предатель взорвался!"
+
+--Freeze
+-- L["ttt2mg_freeze_desc"] = "Все невинные будут заморожены (и приобретать иммунитет) каждые {time} сек."
+L["ttt2mg_freeze_epop_1"] = "Зима близко"
+L["ttt2mg_freeze_epop_2"] = "Пришествие Ледяного Человека"
+L["ttt2mg_freeze_epop_3"] = "В этой вселенной есть только одно абсолютное: всё замерзает!"
+L["ttt2mg_freeze_epop_4"] = "Сегодня ночью ад замерзает."
+L["ttt2mg_freeze_epop_5"] = "Боюсь, моё состояние оставило меня равнодушным к твоей милости."
+L["ttt2mg_freeze_epop_6"] = "Крутая вечеринка."
+L["ttt2mg_freeze_epop_7"] = "Ты не отправишь меня в холодильник."
+L["ttt2mg_freeze_epop_8"] = "Держись спокойно, птичий мальчик."
+L["ttt2mg_freeze_epop_9"] = "Парни, всем надо успокоиться!"
+L["ttt2mg_freeze_epop_10"] = "Это холодный город."
+L["ttt2mg_freeze_epop_11"] = "Сегодняшний прогноз: мороз приближается!"
+L["ttt2mg_freeze_epop_12"] = "Что убило динозавров? Ледниковый период!"
+L["ttt2mg_freeze_epop_13"] = "Давай бросим немного льда!"
+L["ttt2mg_freeze_epop_14"] = "Чувствуешь, как он приближается? Ледяной холод космоса!"
+L["ttt2mg_freeze_epop_15"] = "Замерзай в аду, Бэтмен!"
+
+--Multigame
+L["ttt2mg_randomxn_epop"] = "Случайно x{count}"
+
+--Privacy
+L["ttt2mg_privacy_epop"] = "{role} купил {item}."
+
+--Soulmates
+L["ttt2mg_soulmate_yours"] = "Ваша родственная душа {nick}."
+L["ttt2mg_soulmate_none"] = "У вас нет родственной души. :("
+L["ttt2mg_soulmate_pair"] = "{nick1} и {nick2} теперь родственные души."
+
+--Suspicion
+L["ttt2mg_suspicion_epop_sus"] = "{nick} кажется подозрительным..."
+L["ttt2mg_suspicion_epop_jes"] = "{nick} шут"
+L["ttt2mg_suspicion_epop_tra"] = "{nick} предатель"
+
+--Switch
+L["ttt2mg_switch_epop"] = "Go!"
+
+--You can only jump once
+L["ttt2mg_jump_epop"] = "{nick} дважды пытался прыгнуть..."
